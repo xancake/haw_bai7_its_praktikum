7d4f6e6058b12daa0bf38dbd4e75d403f26f0e7d
Xancake <xanylein@web.de>
2017-05-12 15:38:32 +0200

    Praktikum 3:
    - Nutzen des KeyFileWriters
    - Parameterisierung hinzugefügt

--- src/org/haw/its/praktikum3/RSAKeyCreator.java
+++ src/org/haw/its/praktikum3/RSAKeyCreator.java
@@ -1,20 +1,15 @@
 package org.haw.its.praktikum3;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import org.haw.its.praktikum3.fileio.KeyFileWriter;
 
 public class RSAKeyCreator {
 	public static void main(String[] args) throws Exception {
-		String user = "lars";
+		String user = getUsernameFromArgs(args);
 		
 		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
 		keyGenerator.initialize(2048);
@@ -25,17 +20,30 @@ public class RSAKeyCreator {
 		Key publicKey   = x509.translateKey(keyPair.getPublic());
 		Key privateKey  = pkcs8.translateKey(keyPair.getPrivate());
 		
-		try (DataOutputStream os = new DataOutputStream(new FileOutputStream("res/praktikum3/" + user + ".pub.txt"))) {
-			os.writeInt(user.length());
-			os.write(user.getBytes());
-			os.writeInt(publicKey.getEncoded().length);
-			os.write(publicKey.getEncoded());
+		KeyFileWriter keyFileWriter = new KeyFileWriter("res/praktikum3");
+		keyFileWriter.writeKeyFile(user, publicKey.getEncoded(), "pub");
+		keyFileWriter.writeKeyFile(user, privateKey.getEncoded(), "prv");
+	}
+	
+	private static String getUsernameFromArgs(String[] args) {
+		String user = null;
+		switch(args.length) {
+			case 0: {
+				try(Scanner key = new Scanner(System.in)) {
+    				System.out.print("Benutzername: ");
+    				user = key.nextLine();
+				}
+				break;
+			}
+			case 1: {
+				user = args[0];
+				break;
+			}
+			default: {
+				System.out.println("Usage: java RSAKeyCreator <username>");
+				System.exit(1);
+			}
 		}
-		try (DataOutputStream os = new DataOutputStream(new FileOutputStream("res/praktikum3/" + user + ".prv.txt"))) {
-			os.write(user.length());
-			os.write(user.getBytes());
-			os.write(privateKey.getEncoded().length);
-			os.write(privateKey.getEncoded());
-		}
+		return user;
 	}
 }
